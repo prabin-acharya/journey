cb8ffad96d69499414977a14d5d0abf78efefb3d
Prabin <acharyaprabin101@gmail.com>
2021-08-06 09:06:48 +0545

    removed Files,DailyJournal and added Button comp in Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,33 @@
-import DailyJournal from "./DailyJournal";
 import Searchbar from "./Searchbar";
-import Files from "./Files";
+import Button from "./Button";
 
 const Sidebar = ({ files, onClick }) => {
+  const onClicked = (file) => onClick(file);
+  const journal = {
+    id: 0,
+    title: "My Journal",
+    content: "TODO: Complete the Journal Project.",
+  };
+
   return (
     <div className="sidebar">
       <h2>Prabin's journey</h2>
+
       <Searchbar />
-      <DailyJournal onClick={onClick} />
-      <Files files={files} onClick={onClick} />
+
+      <Button text="Daily Journal" onClick={() => onClicked(journal)} />
+
+      <div className="files">
+        <ul>
+          {files.map((file) => (
+            <Button
+              key={file.id}
+              text={file.title}
+              onClick={() => onClicked(file)}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
