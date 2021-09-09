55d2936bc7facc5f5f1719858fffb26e5c0dc1b6
prabin-acharya <acharyaprabin101@gmail.com>
2021-09-09 11:40:10 +0545

    Add char Limit to Page title in Sidebar

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,4 +1,8 @@
 const Button = ({ text, onClick, id }) => {
+  const charLimit = 27;
+  if (text.length > charLimit) {
+    text = text.substring(0, charLimit) + "...";
+  }
   return (
     <div>
       <button id={id} onClick={onClick}>
