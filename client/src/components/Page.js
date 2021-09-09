ec40ab9b1d02a62eadd353103645a1f344e75817
prabin-acharya <acharyaprabin101@gmail.com>
2021-09-09 14:04:30 +0545

    Add Edit Page icon,Add Outline in textarea

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import EditPage from "./EditPage";
+import { BiPencil } from "react-icons/bi";
 
 const Page = ({ page, clickPage, fetchPages }) => {
   const [editStatus, setEditStatus] = useState(false);
   return (
     <div className="page">
-      {!editStatus && <h1>{page.title}</h1>}
-      <button onClick={() => setEditStatus(!editStatus)}>Edit</button>
-      {editStatus && (
-        <EditPage
-          page={page}
-          clickPage={clickPage}
-          fetchPages={fetchPages}
-          setEditStatus={setEditStatus}
-        />
-      )}
-      <br />
-      {!editStatus && page.content}
+      <div className="page-heading">
+        {!editStatus && <h1>{page.title}</h1>}
+        <button onClick={() => setEditStatus(!editStatus)}>
+          {!editStatus && <BiPencil />}
+        </button>
+        {editStatus && (
+          <EditPage
+            page={page}
+            clickPage={clickPage}
+            fetchPages={fetchPages}
+            setEditStatus={setEditStatus}
+          />
+        )}
+      </div>
+      {!editStatus && <p>{page.content}</p>}
+
+      {/* <h1>{page.title}</h1> */}
     </div>
   );
 };
