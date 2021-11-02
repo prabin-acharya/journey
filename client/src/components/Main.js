176a2700d6c0d8216b30ae91d9d785ecb53d4c20
Prabin <acharyaprabin101@gmail.com>
2021-11-02 18:22:03 +0545

    User-profile Dropdown template

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,45 @@
+import { useState, useEffect, useRef } from "react";
 import AddPage from "./AddPage";
 import Page from "./Page";
 import Journal from "./Journal";
 import { FiMoreHorizontal } from "react-icons/fi";
 
 const Main = ({ page, setOpenPage, fetchPages, search }) => {
+  const [showUserProfile, setShowUserProfile] = useState(false);
+  const userProfileRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (
+      userProfileRef.current &&
+      !userProfileRef.current.contains(event.target)
+    ) {
+      setShowUserProfile(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="main">
-      <FiMoreHorizontal size={35} className="more-icon" />
+      <FiMoreHorizontal
+        onClick={() => setShowUserProfile(true)}
+        size={30}
+        className="more-icon"
+      />
+      {showUserProfile && (
+        <div ref={userProfileRef} className="user-profile-dropdown">
+          <h2>User Full Name</h2>
+          useremail@domain.com
+          <br />
+          Log Out
+        </div>
+      )}
+
       {page === "Journal" ? (
         <Journal search={search} />
       ) : page === "AddPage" ? (
