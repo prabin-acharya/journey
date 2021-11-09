b9167ba982c2dc47abfbe949d2893893eea12b8d
Prabin <acharyaprabin101@gmail.com>
2021-11-09 13:16:38 +0545

    Style user dropdown

--- client/src/components/UserDropdown.js
+++ client/src/components/UserDropdown.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { BiLogOut } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
 
 const UserDropdown = ({ user, setAuthStatus }) => {
   return (
     <div className="user-profile-dropdown">
-      <h2>{user.name}</h2>
-      {user.email}
-      <br />
-      Log Out
-      <BiLogOut
+      <div className="test-user">
+        <div className="user-details">
+          <span className="user-profile-icon">
+            <CgProfile size={30} />
+          </span>
+          <span clasname="name-email">
+            <span className="user-name">{user.name}</span>
+            <br />
+            <span className="user-email">{user.email}</span>
+          </span>
+        </div>
+      </div>
+      <div
+        className="user-logout"
         onClick={() => {
           localStorage.removeItem("token");
           setAuthStatus(false);
         }}
-      />
+      >
+        Log Out
+      </div>
     </div>
   );
 };
