ea98ca3b10f3d078b0b12cbc805ade781a32f267
Prabin <acharyaprabin101@gmail.com>
2021-11-28 23:25:03 +0545

    Frontend for topics in Add Page

--- client/src/components/AddPage.js
+++ client/src/components/AddPage.js
@@ -4,6 +4,7 @@ import React from "react";
 const AddPage = ({ fetchPages, setOpenPage }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [topics, setTopics] = useState("");
 
   //Add Page
   const addPage = (page) => {
@@ -23,29 +24,54 @@ const AddPage = ({ fetchPages, setOpenPage }) => {
       .catch((err) => console.log(err));
   };
 
+  const addTopics = (newtopics) => {
+    if (newtopics.length > topics.length) {
+      if (newtopics.substr(newtopics.length - 1) === ",") {
+        newtopics = newtopics + " ";
+        setTopics(newtopics);
+      } else {
+        setTopics(newtopics);
+      }
+    } else {
+      setTopics(newtopics);
+    }
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    addPage({ title, content });
+    const arrayTopics = topics.split(", ");
+    addPage({ title, content, arrayTopics });
   };
 
   return (
     <form className="addpage" onSubmit={onSubmit}>
-      <div className="form-control-title">
-        <input
-          className="title"
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <textarea
-          type="text"
-          placeholder="Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
+      <div className="page">
+        <div className="form-control-title">
+          <input
+            className="title"
+            type="text"
+            placeholder="Untitled"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-control-topics">
+          <input
+            className="title"
+            type="text"
+            placeholder="Add Relevant topics.."
+            value={topics}
+            onChange={(e) => addTopics(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <textarea
+            type="text"
+            placeholder="Start writing here..."
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
       </div>
       <button disabled={!title.trim()} type="submit" className="btn">
         Save Page
