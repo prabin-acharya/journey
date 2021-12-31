a76a037dc8852e4c17ba8ff15caad458f12c67df
Prabin Acharya <acharyaprabin101@gmail.com>
2021-12-31 18:52:45 +0545

    navigate to page after edit

--- client/src/components/EditPage.js
+++ client/src/components/EditPage.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
-const EditPage = ({ page, setOpenPage, fetchPages, setEditStatus }) => {
+const EditPage = ({ page, fetchPages }) => {
   const [title, setTitle] = useState(page.title);
   const [content, setContent] = useState(page.content);
   const [topics, setTopics] = useState(
     page.topics ? page.topics.toString().replace(/,/g, ", ") : ""
   );
+  const navigate = useNavigate();
 
   //add space after comma
   const addTopics = (newtopics) => {
@@ -42,53 +44,48 @@ const EditPage = ({ page, setOpenPage, fetchPages, setEditStatus }) => {
     })
       .then(() => {
         fetchPages().then(() => {
-          setOpenPage(newPage);
-          setEditStatus(false);
+          navigate(`/${page.title}`);
         });
       })
       .catch((err) => console.log(err));
   };
 
-  useEffect(() => {
-    return () => {
-      setEditStatus(false);
-    };
-  });
-
   return (
-    <form className="editpage" onSubmit={onSubmit}>
-      <div className="page">
-        <div className="form-control-title">
-          <input
-            className="title"
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+    <div className="main">
+      <form className="editpage" onSubmit={onSubmit}>
+        <div className="page">
+          <div className="form-control-title">
+            <input
+              className="title"
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="form-control-topics">
+            <input
+              className="title"
+              type="text"
+              placeholder="Add Relevant topics.."
+              value={topics}
+              onChange={(e) => addTopics(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <textarea
+              type="text"
+              placeholder="Content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="form-control-topics">
-          <input
-            className="title"
-            type="text"
-            placeholder="Add Relevant topics.."
-            value={topics}
-            onChange={(e) => addTopics(e.target.value)}
-          />
-        </div>
-        <div className="form-control">
-          <textarea
-            type="text"
-            placeholder="Content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-      </div>
-      <button type="submit" className="btn">
-        Save Page
-      </button>
-    </form>
+        <button type="submit" className="btn">
+          Save Page
+        </button>
+      </form>
+    </div>
   );
 };
 
--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,43 +1,28 @@
-import { useState } from "react";
-import EditPage from "./EditPage";
+import { Link } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import { BiPencil } from "react-icons/bi";
 
-const Page = ({ page, setOpenPage, fetchPages }) => {
-  const [editStatus, setEditStatus] = useState(false);
-
+const Page = ({ page }) => {
   return (
     <div className="main">
-      {editStatus ? (
-        <EditPage
-          page={page}
-          setOpenPage={setOpenPage}
-          fetchPages={fetchPages}
-          setEditStatus={setEditStatus}
-        />
-      ) : (
-        <div className="page">
-          <span className="page-heading">
-            <h1>{page.title}</h1>
-            <button
-              id="page-edit-icon"
-              onClick={() => setEditStatus(!editStatus)}
-            >
-              <BiPencil />
-            </button>
-          </span>
-          <div className="page-topics">
-            {page.topics &&
-              page.topics.map((topic) => (
-                <span className="page-topic">#{topic}</span>
-              ))}
-          </div>
-
-          <span className="page-content">
-            <ReactMarkdown children={page.content} />
-          </span>
+      <div className="page">
+        <span className="page-heading">
+          <h1>{page.title}</h1>
+          <Link to={`/${page.title}/edit`}>
+            <BiPencil className="page-edit-icon" />
+          </Link>
+        </span>
+        <div className="page-topics">
+          {page.topics &&
+            page.topics.map((topic) => (
+              <span className="page-topic">#{topic}</span>
+            ))}
         </div>
-      )}
+
+        <span className="page-content">
+          <ReactMarkdown children={page.content} />
+        </span>
+      </div>
     </div>
   );
 };
